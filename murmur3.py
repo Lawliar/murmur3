17af45a8f288c63f56060626e651ff099bec21b4
Changming Liu <charley.ashbringer@gmail.com>
2024-08-20 16:16:12 -0400

    add ctypes wrapped

---
+++ murmur3.py
@@ -0,0 +1,42 @@
+import ctypes,os
+import numpy as np
+dir_path = os.path.dirname(os.path.realpath(__file__))
+_embhash = ctypes.cdll.LoadLibrary("{}/libmurmur3.so".format(dir_path))
+def murmur32_128(number,seed,mapsize):
+    global _embhash
+    
+    c_int32_p_ty = ctypes.POINTER(ctypes.c_int32)
+
+    wrapped_key = ctypes.c_int32(number)
+    key = ctypes.cast(ctypes.addressof(wrapped_key), c_int32_p_ty)
+    
+    len = ctypes.c_int32(4)
+
+    seed = ctypes.c_int32(seed)
+
+    np_output_array = np.arange(3,dtype=np.int32)
+    output = np_output_array.ctypes.data_as(c_int32_p_ty)
+    #output = ctypes.cast(ctypes.addressof(wrapped_output), c_int128_p_ty)
+
+    _embhash.MurmurHash3_x86_128(key,len,seed,output)
+    
+    return 
+def murmur32_32(number,seed, mapsize):
+    global _embhash
+    
+    c_int32_p_ty = ctypes.POINTER(ctypes.c_int32)
+
+    wrapped_key = ctypes.c_int32(number)
+    key = ctypes.cast(ctypes.addressof(wrapped_key), c_int32_p_ty)
+    
+    len = ctypes.c_int32(4)
+
+    seed = ctypes.c_int32(seed)
+
+    wrapped_output = ctypes.c_int32(0)
+    output = ctypes.cast(ctypes.addressof(wrapped_output), c_int32_p_ty)
+
+    _embhash.MurmurHash3_x86_32(key,len,seed,output)
+    return 
+if __name__ == '__main__':
+    print(murmur32_128(344,0x12331234,32))
